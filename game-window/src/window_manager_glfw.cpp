5f0641f1eda666abea93fcb9a6f07b768f9dc4a2
Ryster <ry.diffusion@proton.me>
2024-07-13 18:05:57 -0300

    feat: make glfw errors more descriptive

--- game-window/src/window_manager_glfw.cpp
+++ game-window/src/window_manager_glfw.cpp
@@ -1,29 +1,35 @@
-#include <stdexcept>
 #include "window_manager_glfw.h"
-#include "window_glfw.h"
 #include "joystick_manager_glfw.h"
+#include "window_glfw.h"
+#include <stdexcept>
 
 GLFWWindowManager::GLFWWindowManager() {
-    if (glfwInit() != GLFW_TRUE)
-        throw std::runtime_error("glfwInit error");
-    GLFWJoystickManager::init();
+  if (glfwInit() != GLFW_TRUE) {
+    const char *description;
+    int code = glfwGetError(&description);
+
+    throw std::runtime_error("Failed to init GLFW: " +
+                             std::string(description));
+  }
+  GLFWJoystickManager::init();
 }
 
 GameWindowManager::ProcAddrFunc GLFWWindowManager::getProcAddrFunc() {
-    return (GameWindowManager::ProcAddrFunc) glfwGetProcAddress;
+  return (GameWindowManager::ProcAddrFunc)glfwGetProcAddress;
 }
 
-std::shared_ptr<GameWindow> GLFWWindowManager::createWindow(const std::string& title, int width, int height,
-                                                             GraphicsApi api) {
-    return std::shared_ptr<GameWindow>(new GLFWGameWindow(title, width, height, api));
+std::shared_ptr<GameWindow>
+GLFWWindowManager::createWindow(const std::string &title, int width, int height,
+                                GraphicsApi api) {
+  return std::shared_ptr<GameWindow>(
+      new GLFWGameWindow(title, width, height, api));
 }
 
 void GLFWWindowManager::addGamepadMappingFile(const std::string &path) {
-    GLFWJoystickManager::loadMappingsFromFile(path);
+  GLFWJoystickManager::loadMappingsFromFile(path);
 }
-
 
 // Define this window manager as the used one
 std::shared_ptr<GameWindowManager> GameWindowManager::createManager() {
-    return std::shared_ptr<GameWindowManager>(new GLFWWindowManager());
+  return std::shared_ptr<GameWindowManager>(new GLFWWindowManager());
 }
